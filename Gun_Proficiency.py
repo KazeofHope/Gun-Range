279eaeaddb0adfe1ea142282c449b144ba991f3f
KazeofHope <51674169+KazeofHope@users.noreply.github.com>
2020-08-28 20:04:56 -0400

    Add files via upload

--- Gun_Proficiency.py
+++ Gun_Proficiency.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Testing_Ground:
     def __init__(self):
@@ -257,14 +259,11 @@ class Testing_Ground:
     def exporting_data(self, filepath):
         for i in self.prev_data.get("Shooting Range Data"):
             if self.current_gun in (self.prev_data.get("Shooting Range Data" ).get(i)):
-                # self.prev_data.get("Shooting Range Data" ).get(i).get(self.current_gun).get("Data").update({self.current_training_mode :
-                t = self.prev_data.get("Shooting Range Data" ).get(i).get(self.current_gun).get("Data").get(self.current_training_mode).extend(self.new_data)
-                print(t)
+                self.prev_data.get("Shooting Range Data" ).get(i).get(self.current_gun).get("Data").get(self.current_training_mode).extend(self.new_data)
 
-        new_data = self.prev_data # updated data
+        new_data = self.prev_data
 
         with open(f'{filepath}', 'w') as json_file:
-            print(new_data)
             json.dump(new_data, json_file)
 
     def practice_session_start(self):
@@ -320,7 +319,7 @@ class Testing_Ground:
             elif i > 0:
                 print(f"Training Round {i}")
                 while True:
-                    n = int(input("Input Score "))
+                    n = int(input("Input Score: "))
                     if n > -1 and n < 31:
                         break
                     else:
@@ -358,7 +357,105 @@ class Testing_Ground:
         if user_input == "Y":
             self.exporting_data(self.associated_filepath)
         else:
-            return f"New Scores for {self.current_gun}: {self.new_data}"
+            print(f"New Scores for {self.current_gun}: {self.new_data}")
+
+    def visualize_all_scores(self):
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate('{}'.format(height),
+                            xy=(rect.get_x() + rect.get_width() / 2, height),
+                            xytext=(0, 3),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='center', va='bottom')
+
+        easy_data = []
+        medium_data = []
+        hard_data = []
+        labels = []
+
+        for i in self.prev_data.get("Shooting Range Data"):
+            for j in self.prev_data.get("Shooting Range Data").get(i):
+                labels.append(j)
+                a = len(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Easy"))
+                b = len(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Medium"))
+                c = len(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Hard"))
+                if a > 0:
+                    easy_data.append(sum(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Easy"))/a)
+                else:
+                    easy_data.append(0)
+
+                if b > 0:
+                    medium_data.append(sum(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Medium"))/b)
+                else:
+                    medium_data.append(0)
+
+                if c > 0:
+                    hard_data.append(sum(self.prev_data.get("Shooting Range Data").get(i).get(j).get("Data").get("Hard"))/c)
+                else:
+                    hard_data.append(0)
+
+        X = np.arange(len(labels))
+        width = 0.25
+
+        fig, ax = plt.subplots()
+        ax.bar(X - width/2, easy_data, width, label= 'Easy', color= 'g')
+        ax.bar(X + width/2, medium_data, width, label= 'Medium', color= 'y')
+        ax.bar(X + 3*width/2, hard_data, width, label= 'Hard', color= 'r')
+
+        ax.set_ylabel('Scores')
+        ax.set_title('Scores by Gun')
+        plt.ylim(0, 30)
+        ax.set_xticks(X + width / 2)
+        ax.set_xticklabels(labels)
+        plt.grid(color='grey', linestyle='-', linewidth=0.25)
+        ax.legend()
+
+        plt.show()
+
+    def visualize_category(self, category = None):
+        labels = []
+        easy_data = []
+        medium_data = []
+        hard_data = []
+        sect_data = self.prev_data.get("Shooting Range Data").get(category)
+        for i in sect_data:
+            labels.append(i)
+            if len(sect_data.get(i).get("Data").get("Easy")) > 0:
+                easy_data.append(sum(sect_data.get(i).get("Data").get("Easy"))/len(sect_data.get(i).get("Data").get("Easy")))
+            else:
+                easy_data.append(0)
+
+            if len(sect_data.get(i).get("Data").get("Medium")) > 0:
+                medium_data.append(sum(sect_data.get(i).get("Data").get("Medium"))/len(sect_data.get(i).get("Data").get("Medium")))
+            else:
+                medium_data.append(0)
+
+            if len(sect_data.get(i).get("Data").get("Hard")) > 0:
+                hard_data.append(sum(sect_data.get(i).get("Data").get("Hard"))/len(sect_data.get(i).get("Data").get("Hard")))
+            else:
+                hard_data.append(0)
+
+        X = np.arange(len(labels))
+        width = 0.25
+
+        fig, ax = plt.subplots()
+        ax.bar(X - width/2, easy_data, width, label= 'Easy', color= 'g')
+        ax.bar(X + width/2, medium_data, width, label= 'Medium', color= 'y')
+        ax.bar(X + 3*width/2, hard_data, width, label= 'Hard', color= 'r')
+
+        ax.set_ylabel('Scores')
+        ax.set_title(f'Scores by {category}')
+        plt.ylim(0, 30)
+        ax.set_xticks(X + width / 2)
+        ax.set_xticklabels(labels)
+        plt.grid(color='grey', linestyle='-', linewidth=0.25)
+        ax.legend()
+
+        plt.show()
+
+
 
 
 
@@ -368,4 +465,7 @@ if __name__ == "__main__":
     filepath = r"C:\Users\vsbas\PycharmProjects\ECE homework 241 11\Stupid Projects\gun_skill_data.json"
     training_session = Testing_Ground()
     training_session.download_data(filepath)
+    training_session.visualize_all_scores()
+    # training_session.visualize_category("AR")
     training_session.practice_session_start()
+    training_session.visualize_all_scores()
